c338388822207fdc5ad317337138d15f92a018e7
Brian Ingles <github@emeraldwalk.com>
2020-01-07 07:47:57 -0600

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CustomElement } from '.'
+
+const App: React.FC = () => {
+  return (
+    <div className="c_app">
+      App
+      <CustomElement
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CustomElement.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export interface CustomElementProps {
+}
+
+const CustomElement: React.FC<CustomElementProps> = () => (
+  <div>
+
+  </div>
+)
+
+export default CustomElement
